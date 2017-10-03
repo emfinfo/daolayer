315672646982f1aba0cf702031fb17280beab10d
jcstritt <strittjc@edufr.ch>
2017-10-03 15:37:13 +0200

    Release 5.1.5 (Search2 redesigned)

--- src/main/java/ch/emf/dao/filtering/Search2.java
+++ src/main/java/ch/emf/dao/filtering/Search2.java
@@ -9,7 +9,7 @@ import java.util.List;
  * "inner join", chose que la classe "Search" standard ne permet pas,
  * car elle est basée sur une seule table. Les méthodes de cette classe
  * ajoutent ensuite les différents filtres (pour la clause WHERE avec
- * des AND automatiques entre deux).<br>
+ * des AND automatiques entre deux -modifiable-).<br>
  * <br>
  * Exemple d'utilisation :<br>
  * <pre>
@@ -38,133 +38,301 @@ public class Search2 {
   private String jpql;
   private List<Object> values;
   private int n;
-  private boolean addAnd;
+  private boolean addOp;
+  private String defLogOp;
 
-  public Search2(String jpql) {
+  /**
+   * Constructeur 1.
+   *
+   * @param jpql une chaine de car. représentant las base de la requête JPQL
+   * @param defLogOp opérateur par défaut entre les filtres ("and" ou "or")
+   */
+  public Search2(String jpql, String defLogOp) {
     this.jpql = jpql;
+    this.defLogOp = defLogOp;
     firstResult = -1;
     maxResults = -1;
     values = new ArrayList<>();
     n = 0;
-    addAnd = this.jpql.toLowerCase().contains(" where ");
+    addOp = this.jpql.toLowerCase().contains(" where ");
   }
 
-  private void startOp() {
-    if (addAnd) {
-      jpql += " and ";
+  /**
+   * Constructeur 2.<br>
+   * L'opérateur par défaut entre les filtres est un "ET" ("and").
+   *
+   * @param jpql une chaine de car. représentant las base de la requête JPQL
+   */
+  public Search2(String jpql) {
+    this(jpql, "and");
+  }
+
+  /**
+   * Setter pour modifier l'opérateur logique par défaut ("and") ajouté
+   * avant chaque filtre. S'il est null ou vide, aucun opérateur logique ne sera ajouté.
+   *
+   * @param defLogOp l'opérateur logique par défaut ajouté automatiquement avant chaque filtre
+   */
+
+  public void setDefaultLogicalOp(String defLogOp) {
+    this.defLogOp = defLogOp;
+  }
+
+  /**
+   * Ajoute un simple filtre basé sur un opérateur et une valeur.<br>
+   * Il n'y a pas de test sur le contenu de la valeur du filtre et
+   * il n'y pas non plus d'opérateur logique rajouté avant le filtre lui-même.
+   *
+   * @param op l'opérateur du filtre (ex: "=").
+   * @param prop le nom de la propriété de filtrage
+   * @param value la valeur de test
+   */
+  public void addSimpleFilter(String op, String prop, Object value) {
+    n++;
+    values.add(value);
+    jpql += prop + op + "?" + n;
+  }
+
+  /**
+   * Méthode privée pour ajouter un opérateur logique avant un filtre particuler.
+   */
+  private void addLogicalOp() {
+    if (addOp) {
+      if (defLogOp != null && !defLogOp.isEmpty()) {
+        jpql += " " + defLogOp + " ";
+      }
     } else {
       jpql += " where ";
     }
-    addAnd = true;
+    addOp = true;
   }
 
-  public void addSimpleFilter(String op, String fieldName, Object value) {
-    n++;
-    values.add(value);
-    jpql += fieldName + op + "?" + n;
+  /**
+   * Méthode privée pour ajouter un filtre sans spécification de valeur pour
+   * les opérateurs tels que "is null", "is empty", etc.
+   *
+   * @param op l'opérateur du filtre (ex: "is null").
+   * @param prop le nom de la propriété de filtrage
+   */
+  private void addFilterNoValue(String op, String prop) {
+    addLogicalOp();
+    jpql += prop + " " + op;
   }
 
-  public void addFilter(String op, String fieldName, Object value) {
+  /**
+   * N'ajoute un filtre que si la valeur spécifiée est non nulle.<br>
+   * L'opérateur logique par défaut est rajoutée avant le filtre (sauf s'il est vide);
+   *
+   * @param op l'opérateur du filtre (ex: "=").
+   * @param prop le nom de la propriété de filtrage
+   * @param value la valeur de test
+   */
+  public void addFilter(String op, String prop, Object value) {
     if (value != null) {
-      startOp();
-      addSimpleFilter(op, fieldName, value);
+      addLogicalOp();
+      addSimpleFilter(op, prop, value);
     }
   }
 
-  public void addFilterNoValue(String op, String fieldName) {
-    startOp();
-    jpql += fieldName + " " + op;
+  /**
+   * Ajoute un filtre qui teste une égalité.
+   *
+   * @param prop le nom de la propriété de filtrage
+   * @param value la valeur de test
+   */
+  public void addFilterEqual(String prop, Object value) {
+    addFilter(Operator.EQUAL.getOp(), prop, value);
   }
 
-  public void addFilterEqual(String fieldName, Object value) {
-    addFilter(Operator.EQUAL.getOp(), fieldName, value);
-  }
-
-  public void addFilterEqual(String fieldName, Object value1, Object value2) {
+  /**
+   * Ajoute un filtre qui teste une double égalité avec un opérateur "ou" entre-deux.
+   *
+   * @param prop le nom de la propriété de filtrage
+   * @param value1 la première valeur de test
+   * @param value2 la deuxième valeur de test
+   */
+  public void addFilterEqual(String prop, Object value1, Object value2) {
     if (value1 != null && value2 != null) {
-      startOp();
+      addLogicalOp();
       jpql += "(";
-      addSimpleFilter(Operator.EQUAL.getOp(), fieldName, value1);
+      addSimpleFilter(Operator.EQUAL.getOp(), prop, value1);
       jpql += " or ";
-      addSimpleFilter(Operator.EQUAL.getOp(), fieldName, value2);
+      addSimpleFilter(Operator.EQUAL.getOp(), prop, value2);
       jpql += ")";
     }
   }
 
-  public void addFilterNotEqual(String fieldName, Object value) {
-    addFilter(Operator.NOT_EQUAL.getOp(), fieldName, value);
+  /**
+   * Ajoute un filtre qui teste une inégalité.
+   *
+   * @param prop le nom de la propriété de filtrage
+   * @param value la valeur de test
+   */
+  public void addFilterNotEqual(String prop, Object value) {
+    addFilter(Operator.NOT_EQUAL.getOp(), prop, value);
   }
 
-  public void addFilterLessThan(String fieldName, Object value) {
-    addFilter(Operator.LESS_THAN.getOp(), fieldName, value);
+  /**
+   * Ajoute un filtre qui teste si des valeurs dans la BD sont plus
+   * petites qu'une valeur de référence spécifiée.
+   *
+   * @param prop le nom de la propriété de filtrage
+   * @param value la valeur de référence
+   */
+  public void addFilterLessThan(String prop, Object value) {
+    addFilter(Operator.LESS_THAN.getOp(), prop, value);
   }
 
-  public void addFilterLessOrEqual(String fieldName, Object value) {
-    addFilter(Operator.LESS_OR_EQUAL.getOp(), fieldName, value);
+  /**
+   * Ajoute un filtre qui teste si des valeurs dans la BD sont plus
+   * petites ou égales à une valeur de référence spécifiée.
+   *
+   * @param prop le nom de la propriété de filtrage
+   * @param value la valeur de référence
+   */
+  public void addFilterLessOrEqual(String prop, Object value) {
+    addFilter(Operator.LESS_OR_EQUAL.getOp(), prop, value);
   }
 
-  public void addFilterGreatherThan(String fieldName, Object value) {
-    addFilter(Operator.GREATER_THAN.getOp(), fieldName, value);
+  /**
+   * Ajoute un filtre qui teste si des valeurs dans la BD sont plus
+   * grandes q'une valeur de référence spécifiée.
+   *
+   * @param prop le nom de la propriété de filtrage
+   * @param value la valeur de référence
+   */
+  public void addFilterGreatherThan(String prop, Object value) {
+    addFilter(Operator.GREATER_THAN.getOp(), prop, value);
   }
 
-  public void addFilterGreaterOrEqual(String fieldName, Object value) {
-    addFilter(Operator.GREATER_OR_EQUAL.getOp(), fieldName, value);
+  /**
+   * Ajoute un filtre qui teste si des valeurs dans la BD sont plus
+   * grandes ou égales à une valeur de référence spécifiée.
+   *
+   * @param prop le nom de la propriété de filtrage
+   * @param value la valeur de référence
+   */
+  public void addFilterGreaterOrEqual(String prop, Object value) {
+    addFilter(Operator.GREATER_OR_EQUAL.getOp(), prop, value);
   }
 
-  public void addFilterBetween(String fieldName, Object startValue, Object endValue) {
-    addFilter(" " + Operator.BETWEEN.getOp() + " ", fieldName, startValue);
+  /**
+   * Ajoute un filtre qui teste si une valeur dans la DB se trouve
+   * entre deux valeurs de références pour une propriété donnée.
+   *
+   * @param prop le nom de la propriété de filtrage
+   * @param startValue la valeur de référence inférieure
+   * @param endValue la valeur de référence supérieure
+   */
+  public void addFilterBetween(String prop, Object startValue, Object endValue) {
+    addFilter(" " + Operator.BETWEEN.getOp() + " ", prop, startValue);
     addFilter("", "", endValue);
   }
 
-  public void addFilterLike(String fieldName, String s) {
-    addFilter(" " + Operator.LIKE.getOp() + " ", fieldName, s);
+  /**
+   * Ajoute un filtre qui teste si des valeurs dans la BD ressemblent (like)
+   * à une valeur de référence spécifiée pour une propriété donnée.
+   *
+   * @param prop le nom de la propriété de filtrage
+   * @param value la valeur de référence
+   */
+  public void addFilterLike(String prop, String value) {
+    addFilter(" " + Operator.LIKE.getOp() + " ", prop, value);
   }
 
-  public void addFilterIsNull(String fieldName) {
-    addFilterNoValue(Operator.IS_NULL.getOp(), fieldName);
+  /**
+   * Ajoute un filtre qui teste si les valeurs d'une propriété spécifiée sont nulles.
+   *
+   * @param prop le nom de la propriété de filtrage
+   */
+  public void addFilterIsNull(String prop) {
+    addFilterNoValue(Operator.IS_NULL.getOp(), prop);
   }
 
-  public void addFilterIsNotNull(String fieldName) {
-    addFilterNoValue(Operator.IS_NOT_NULL.getOp(), fieldName);
+  /**
+   * Ajoute un filtre qui teste si les valeurs d'une propriété spécifiée sont non nulles.
+   *
+   * @param prop le nom de la propriété de filtrage
+   */
+  public void addFilterIsNotNull(String prop) {
+    addFilterNoValue(Operator.IS_NOT_NULL.getOp(), prop);
   }
 
-  public void addFilterIsEmpty(String fieldName) {
-    addFilterNoValue(Operator.IS_EMPTY.getOp(), fieldName);
+  /**
+   * Ajoute un filtre qui teste si les valeurs d'une propriété spécifiée sont vides (empty).
+   *
+   * @param prop le nom de la propriété de filtrage
+   */
+  public void addFilterIsEmpty(String prop) {
+    addFilterNoValue(Operator.IS_EMPTY.getOp(), prop);
   }
 
-  public void addFilterIsNotEmpty(String fieldName) {
-    addFilterNoValue(Operator.IS_NOT_EMPTY.getOp(), fieldName);
+  /**
+   * Ajoute un filtre qui teste si les valeurs d'une propriété spécifiée sont non vides (not empty).
+   *
+   * @param prop le nom de la propriété de filtrage
+   */
+  public void addFilterIsNotEmpty(String prop) {
+    addFilterNoValue(Operator.IS_NOT_EMPTY.getOp(), prop);
   }
 
+  /**
+   * Ajoute une condition logique ET (and) après les filtres existants.
+   */
   public void addFilterAnd() {
     jpql += " and ";
   }
 
+  /**
+   * Ajoute une condition logique OU (or) après les filtres existants.
+   */
   public void addFilterOr() {
     jpql += " or ";
   }
 
+  /**
+   * Ajoute une parenthèse ouvrante à la clause WHERE en cours de création.
+   */
   public void addFilterOpenParenthesis() {
     jpql += "(";
   }
 
+  /**
+   * Ajoute une parenthèse fermante à la clause WHERE en cours de création.
+   */
   public void addFilterCloseParenthesis() {
     jpql += ")";
   }
 
-  public void addGroupByField(String fieldName) {
-    jpql += " group by " + fieldName;
+  /**
+   * Ajoute une expression de regroupement (groupe by) après les filtres déjà en cours.
+   *
+   * @param prop le nom de la propriété sur laquelle il faut regrouper
+   */
+  public void addGroupByField(String prop) {
+    jpql += " group by " + prop;
   }
 
-  public void addHavingCondition( String condition ) {
+  /**
+   * Ajoute une expression "qui contient" (having) après les filtres déjà en cours.
+   *
+   * @param condition la condition après le mot-clé "having"
+   */
+  public void addHavingCondition(String condition) {
     jpql += " having " + condition;
   }
 
-  public void addSortFields(String... fieldNames) {
-    if (fieldNames != null) {
+  /**
+   * Ajoute une ou plusieurs propriétés de tri après la requête en cours.<br>
+   * Exemple: "NOM ASC", "NPA DESC".
+   *
+   * @param propNames un tableau de propriétés de tri optionnelles
+   */
+  public void addSortFields(String... propNames) {
+    if (propNames != null) {
       jpql += " order by ";
       int i = 0;
-      for (String fieldName : fieldNames) {
+      for (String fieldName : propNames) {
         if (i > 0) {
           jpql += ",";
         }
@@ -174,27 +342,60 @@ public class Search2 {
     }
   }
 
+  /**
+   * Getter pour l'attribut "index du premier résultat" (firstResult).<br>
+   * Cela permet la pagination des résultats à retrouver.
+   *
+   * @return un entier exprimant l'index du premier résultat
+   */
   public int getFirstResult() {
     return firstResult;
   }
 
+  /**
+   * Setter pour l'attribut "index du premier résultat" (firstResult).<br>
+   * Cela permet la pagination des résultats à retrouver.
+   *
+   * @param firstResult l'index du premier résutat à retourner
+   */
   public void setFirstResult( int firstResult ) {
     this.firstResult = firstResult;
   }
 
+  /**
+   * Getter pour l'attribut "nombre maximal de résultats attendus" (maxResults).<br>
+   * Cela permet la pagination des résultats à retrouver.
+   *
+   * @return un entier exprimant le nombre maximal de résultats attendus
+   */
   public int getMaxResults() {
     return maxResults;
   }
 
-  public void setMaxResults( int maxResults ) {
+/**
+   * Setter pour l'attribut "nb maximal de résultats attendu" (maxResults).<br>
+   * Cela permet la pagination des résultats à retrouver.
+   *
+   * @param maxResults le nombre maximal de résultats attendus dans une requête
+   */
+    public void setMaxResults( int maxResults ) {
     this.maxResults = maxResults;
   }
 
+  /**
+   * Permet de récupérer la requête jpql juste avant que les valeurs de paramètres ne soient injectées.
+   *
+   * @return la requête jpql en cours
+   */
   public String getJpql() {
-//    System.out.println("*** JPQL = " + jpql);
     return jpql;
   }
 
+  /**
+   * Un tableau avec les valeurs à injecter comme parmètres dans la requête jpql en cours.
+   *
+   * @return un tableau de valeurs (Objects)
+   */
   public Object[] getParams() {
     Object[] params = values.toArray();
     return params;
